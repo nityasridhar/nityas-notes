da156f6db98e4200876a46c0114002b7e7883490
Nitya Sridhar <nitya.srid@gmail.com>
2026-01-04 17:55:50 -0800

    strava handler

--- api/strava.js
+++ api/strava.js
@@ -1,3 +1,12 @@
+
 export default function handler(req, res) {
-    res.status(200).json({ message: "Hello from Strava API!" });
-}
+    const clientId = process.env.STRAVA_CLIENT_ID;
+    const clientSecret = process.env.STRAVA_CLIENT_SECRET;
+    const refreshToken = process.env.STRAVA_REFRESH_TOKEN;
+    
+    res.status(200).json({ 
+      hasClientId: !!clientId,
+      hasClientSecret: !!clientSecret,
+      hasRefreshToken: !!refreshToken
+    });
+  }
